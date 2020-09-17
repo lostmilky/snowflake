6aa31620245d8b21a7e8867cb1083eee7d9619b2
yong <zhuyongdan@zhongmei66.com>
2020-09-17 17:33:05 +0800

    lock联调

--- src/Snowflake.php
+++ src/Snowflake.php
@@ -8,16 +8,17 @@ class Snowflake
     public $center_id = 1;              // IDC 机房 id
     public $server_id = 1;              // 机器 id
     public $start_micro_time = 0;       // 开始时间的毫秒级时间戳
-    public $current_micro_time = 0;     // 当前的毫秒级时间戳
+
+    public $lock;
 
     public function __construct()
     {
         $this->center_id = intval(config('snowflake.center_id', 1) );
         $this->server_id = intval(config('snowflake.server_id', 1) );
         $this->start_micro_time = intval(config('snowflake.start_micro_time', 0) );
-        $this->current_micro_time = $this->getMicroTime();
 
         $this->checkConfig();
+        $this->lock = new LocalLock();
     }
 
     public function checkConfig()
@@ -30,7 +31,7 @@ class Snowflake
             throw new \Exception('center_id muster between 1 and 32', 501);
         }
 
-        if($this->start_micro_time > $this->current_micro_time || $this->start_micro_time < 0) {
+        if($this->start_micro_time > $this->getMicroTime() || $this->start_micro_time < 0) {
             throw new \Exception('start_micro_time is out of order', 501);
         }
     }
@@ -59,33 +60,32 @@ class Snowflake
     }
 
 
-    public function getTimeSequence()
+    public function getTimeSequence($current_micro_time)
     {
-        $this->current_micro_time = $this->getMicroTime();
-        return $this->current_micro_time - $this->start_micro_time;
+        return $current_micro_time - $this->start_micro_time;
     }
 
 
     public function snId()
     {
-        $lock = new LocalLock();
-        $lock->lock('s');
+        $this->lock->lock('s');
         $key = ftok(__FILE__, "K");
         $shmid = shmop_open($key, 'c', 0644, 18);
         $cache = shmop_read($shmid, 0, 18);
         $arr = explode('-', $cache);
+        $current_micro_time = $this->getMicroTime();
 
         if(2 != count($arr) ) {
             $seq_id = 1000;   // 由于 shmop_write 的特性所以需要默认4位数字
         } else {
             $cache_time = (float)$arr[0];
             $cache_id = $arr[1];
-            if ($cache_time > $this->current_micro_time){
+            if ($cache_time > $current_micro_time){
                 throw new \Exception('Timestamp is out of order', 500);
-            } elseif ($cache_time == $this->current_micro_time) {
+            } elseif ($cache_time == $current_micro_time) {
                 if($cache_id >= 5095) {
-                    while($cache_time == $this->current_micro_time) {
-                        $this->current_micro_time = $this->getMicroTime();
+                    while($cache_time == $current_micro_time) {
+                        $current_micro_time = $this->getMicroTime();
                     }
                     $seq_id = 1000;
                 } else {
@@ -97,13 +97,15 @@ class Snowflake
         }
 
         // 记录序列ID
-        shmop_write($shmid, $this->current_micro_time.'-'.$seq_id, 0);
+        shmop_write($shmid, $current_micro_time.'-'.$seq_id, 0);
         shmop_close($shmid);
 
-        $lock->unlock('s');
+        $this->lock->unlock('s');
 
         $id = $seq_id - 1000; // 前面由于占位原因是1000开启的，这里取消补偿
-        return (string) ($this->getTimeSequence() << 22 | $this->center_id << 17 | $this->server_id << 12 | $id);
+        $sid = $this->getTimeSequence($current_micro_time);
+
+        return (string) ($sid << 22 | $this->center_id << 17 | $this->server_id << 12 | $id);
     }
 
 
